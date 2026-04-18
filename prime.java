915b03cae496f8c6649e960bb8d7799bdfc29005
Basha <Basha@123@gmail.com>
2026-04-18 13:59:35 +0530

    Added boolean funtion to find Prime number

--- prime.java
+++ prime.java
@@ -3,22 +3,51 @@ class prime
     public static void main(String args[])
     {
         int n =5;
-        int count =1;
 
-        for(int i=2;i<=n;i++)
-        {
-            if(n % i == 0)
-            {
-                count++;
-                break;
-            }
-        }
-        if(count == 2)
-        {
-            System.out.println("Prime number");
-        }else
-        {
-            System.out.println(" Not Prime number");
-        }
+        
+        System.out.println("Prime number Using boolean = "+PrimeUsingBoolean(n));
+        System.out.println("Prime number Using Count = "+PrimeUsingcount(n));
+
     }
+
+        public static void PrimeUsingcount(int n)
+        {
+            int count =1;
+
+             for(int i=2;i<=n;i++)
+            {
+                if(n % i == 0)
+                {
+                    count++;
+                    break;
+                }
+            }
+             if(count == 2)
+            {
+                System.out.println("Prime number");
+            }
+            else
+            {
+                System.out.println(" Not Prime number");
+            }   
+        
+        }
+
+         public static boolean PrimeUsingBoolean(int n)
+         {
+          boolean isboolean =true; 
+
+
+             for(int i=2;i<=n;i++)
+            {
+                if(n % i == 0)
+                {
+                  isboolean = false;
+                    break;
+                }
+            }
+             return isboolean;
+        }
+
+
 }
